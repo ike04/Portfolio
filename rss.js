58aa893b36f5f026b648f8ba65d5db3d98bc02d2
ike04 <boc.0409@outlook.jp>
2023-02-17 23:48:09 +0900

    Qiitaの記事を表示

--- rss.js
+++ rss.js
@@ -1,34 +1,45 @@
 const apiUrl = 'https://qiita.com/api/v2/authenticated_user/items';
-const accessToken = 'YOUR_ACCESS_TOKEN';
+const accessToken = '290836882926e3340d4b599744b50c6364f9509f';
 
 fetch(`${apiUrl}?per_page=5`, {
-    headers: { 'Authorization': `Bearer 290836882926e3340d4b599744b50c6364f9509f` }
+	headers: { 'Authorization': `Bearer ${accessToken}` }
 })
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(item => {
-            const title = item.title;
-            const url = item.url;
-            const body = item.body;
+	.then(response => response.json())
+	.then(data => {
+		const outputList = document.getElementById('Output');
 
-            const article = document.createElement('article');
+		data.forEach(item => {
+			const title = item.title;
+			const url = item.url;
+			const date = new Date(item.created_at).toLocaleDateString();
+			const likesCount = item.likes_count;
+			const iconUrl = item.user.profile_image_url;
 
-            const h2 = document.createElement('h2');
-            h2.textContent = title;
-            article.appendChild(h2);
+			const article = document.createElement('article');
+			article.className = 'output-item';
 
-            const p1 = document.createElement('p');
-            p1.textContent = body;
-            article.appendChild(p1);
+			const img = document.createElement('img');
+			img.src = iconUrl;
+			article.appendChild(img);
 
-            const p2 = document.createElement('p');
-            const a = document.createElement('a');
-            a.href = url;
-            a.textContent = 'Read more';
-            p2.appendChild(a);
-            article.appendChild(p2);
+			const content = document.createElement('div');
+			content.className = 'output-content';
+			article.appendChild(content);
 
-            document.body.appendChild(article);
-        });
-    })
-    .catch(error => console.error(error));
+			const h3 = document.createElement('h3');
+			h3.className = 'output-title';
+			h3.textContent = title;
+			h3.addEventListener('click', () => {
+				window.open(url, '_blank');
+			});
+			content.appendChild(h3);
+
+			const p = document.createElement('p');
+			p.className = 'output-date';
+			p.textContent = `${date} | ${likesCount} LGTM`;
+			content.appendChild(p);
+
+			outputList.appendChild(article);
+		});
+	})
+	.catch(error => console.error(error));
